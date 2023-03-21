f954ce2c9976dba87cfbc54054a983fd980ba95a
unknown <anilkumarravuri510@gmail.com>
2023-03-22 01:07:27 +0530

    auto push

--- robot/components/rabbitmq.sh
+++ robot/components/rabbitmq.sh
@@ -23,16 +23,15 @@ stat $?
 
 rabbitmqctl list_users | grep $APPUSER
 if [ $? -ne 0 ] ; then
-   echo -n "CREATING $COMPONENT APPLICATIN USER :"
+   echo -n "CREATING $COMPONENT APPLICATIoN USER :"
    rabbitmqctl add_user roboshop roboshop123     &>> $LOGFILE
    stat $?
 fi
 
-
-# rabbitmqctl set_user_tags roboshop administrator
-# rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"
-
-# systemctl status rabbitmq-server -l
+echo -n "ADDING REQUIRED PREVILAGES TO THE $APPUSER :"
+rabbitmqctl set_user_tags roboshop administrator
+rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"
+stat $?
 
 
 
