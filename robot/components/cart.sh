8f404fa2b67f1fe9147099389e28fb988ac17843
unknown <anilkumarravuri510@gmail.com>
2023-03-22 00:10:30 +0530

    auto push

--- robot/components/cart.sh
+++ robot/components/cart.sh
@@ -2,69 +2,84 @@
 
 echo "i am cart"
 COMPONENT=cart
-LOGFILE="/tmp/$COMPONENT.log"
-APPUSER=roboshop
 
-#set -e
-#validating wether the executed user is root user or not
-ID=$(id -u)
-if [ "$ID" -ne 0 ] ; then 
-   echo -e "\e[31m you should excute this script as a root user or with sudo as a prefix \e[0m"  
-   exit 1
-fi
+source components/common.sh
 
-stat() {
-if [ $1 -eq 0 ] ; then
-   echo -e "\e[32m success \e[0m"
-else
-   echo -e "\e[31m failure \e[0m"
-   exit 2
-fi
-}
-
-echo -n "CONFIGURING NODEJS REPO :"
-curl --silent --location https://rpm.nodesource.com/setup_16.x |  bash -    &>>  $LOGFILE
-stat $?
-
-echo -n "INSTALLING NODEJS :"
-yum install nodejs -y   &>> $LOGFILE
-stat $?
-
-id $APPUSER   &>> $LOGFILE
-if [ $? -ne 0 ] ; then
-   echo -n " CREATING THE APPLICATION USER ACCOUNT :"
-   useradd $APPUSER    &>> $LOGFILE
-stat $?
-fi
-
-echo -n "DOWNLOADUING THE $COMPONENT :"
-curl -s -L -o /tmp/$COMPONENT.zip "https://github.com/stans-robot-project/$COMPONENT/archive/main.zip" &>> $LOGFILE
-stat $?
-
-echo -n "EXTRACTING THE $COMPONENT IN THE $APPUSER DIRECTORY :"
-cd /home/$APPUSER
-rm -rf /home/$APPUSER/$COMPONENT   &>> $LOGFILE
-unzip -o /tmp/$COMPONENT.zip    &>> $LOGFILE
-stat $?
+NODEJS
 
 
-echo -n "CONFIGURING THE PERMISSIONS :"
-mv /home/$APPUSER/$COMPONENT-main /home/$APPUSER/$COMPONENT
-chown -R $APPUSER:$APPUSER /home/$APPUSER/$COMPONENT
-stat $?
 
-echo -n "INSTALLING $COMPONENT APPLICATION :"
-cd /home/$APPUSER/$COMPONENT/
-npm install    &>>$LOGFILE
-stat $?
 
-echo -n "UPDATING THE SYSTEMD FILES WITH DB DETAILS :"
-sed -i -e 's/REDIS_ENDPOINT/redis.roboshop.internal/' -e 's/CATALOGUE_ENDPOINT/catalogue.roboshop.internal/' /home/$APPUSER/$COMPONENT/systemd.service
-mv /home/$APPUSER/$COMPONENT/systemd.service /etc/systemd/system/$COMPONENT.service
-stat $?
 
-echo -n "STARTING THE $COMPONENT SERVICE :"
-systemctl daemon-reload       &>> $LOGFILE
-systemctl enable $COMPONENT   &>> $LOGFILE
-systemctl restart $COMPONENT     &>> $LOGFILE
-stat $?
+
+
+
+
+
+
+# LOGFILE="/tmp/$COMPONENT.log"
+# APPUSER=roboshop
+
+# #set -e
+# #validating wether the executed user is root user or not
+# ID=$(id -u)
+# if [ "$ID" -ne 0 ] ; then 
+#    echo -e "\e[31m you should excute this script as a root user or with sudo as a prefix \e[0m"  
+#    exit 1
+# fi
+
+# stat() {
+# if [ $1 -eq 0 ] ; then
+#    echo -e "\e[32m success \e[0m"
+# else
+#    echo -e "\e[31m failure \e[0m"
+#    exit 2
+# fi
+# }
+
+# echo -n "CONFIGURING NODEJS REPO :"
+# curl --silent --location https://rpm.nodesource.com/setup_16.x |  bash -    &>>  $LOGFILE
+# stat $?
+
+# echo -n "INSTALLING NODEJS :"
+# yum install nodejs -y   &>> $LOGFILE
+# stat $?
+
+# id $APPUSER   &>> $LOGFILE
+# if [ $? -ne 0 ] ; then
+#    echo -n " CREATING THE APPLICATION USER ACCOUNT :"
+#    useradd $APPUSER    &>> $LOGFILE
+# stat $?
+# fi
+
+# echo -n "DOWNLOADUING THE $COMPONENT :"
+# curl -s -L -o /tmp/$COMPONENT.zip "https://github.com/stans-robot-project/$COMPONENT/archive/main.zip" &>> $LOGFILE
+# stat $?
+
+# echo -n "EXTRACTING THE $COMPONENT IN THE $APPUSER DIRECTORY :"
+# cd /home/$APPUSER
+# rm -rf /home/$APPUSER/$COMPONENT   &>> $LOGFILE
+# unzip -o /tmp/$COMPONENT.zip    &>> $LOGFILE
+# stat $?
+
+
+# echo -n "CONFIGURING THE PERMISSIONS :"
+# mv /home/$APPUSER/$COMPONENT-main /home/$APPUSER/$COMPONENT
+# chown -R $APPUSER:$APPUSER /home/$APPUSER/$COMPONENT
+# stat $?
+
+# echo -n "INSTALLING $COMPONENT APPLICATION :"
+# cd /home/$APPUSER/$COMPONENT/
+# npm install    &>>$LOGFILE
+# stat $?
+
+# echo -n "UPDATING THE SYSTEMD FILES WITH DB DETAILS :"
+# sed -i -e 's/REDIS_ENDPOINT/redis.roboshop.internal/' -e 's/CATALOGUE_ENDPOINT/catalogue.roboshop.internal/' /home/$APPUSER/$COMPONENT/systemd.service
+# mv /home/$APPUSER/$COMPONENT/systemd.service /etc/systemd/system/$COMPONENT.service
+# stat $?
+
+# echo -n "STARTING THE $COMPONENT SERVICE :"
+# systemctl daemon-reload       &>> $LOGFILE
+# systemctl enable $COMPONENT   &>> $LOGFILE
+# systemctl restart $COMPONENT     &>> $LOGFILE
+# stat $?
