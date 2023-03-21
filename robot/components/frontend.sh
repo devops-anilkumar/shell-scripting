b7a29251d86e945cc647dabb5a4097e8c5047c27
unknown <anilkumarravuri510@gmail.com>
2023-03-21 16:46:02 +0530

    auto push

--- robot/components/frontend.sh
+++ robot/components/frontend.sh
@@ -9,7 +9,7 @@ set -e
 #validating wether the executed user is root user or not
 ID=$(id -u)
 if [ "$ID" -ne 0 ] ; then 
-   echo "you should excute this script as a root user or with sudo as a prefix "  
+   echo -e "\e[31m you should excute this script as a root user or with sudo as a prefix \e[0m"  
    exit 1
 fi
 
