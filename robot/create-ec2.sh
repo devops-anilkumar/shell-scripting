796004020c32c5819fe136084dfca5296918cfd1
unknown <anilkumarravuri510@gmail.com>
2023-03-23 00:15:58 +0530

    auto push

--- robot/create-ec2.sh
+++ robot/create-ec2.sh
@@ -14,4 +14,4 @@ AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=DevOps-LabImage-Cen
 echo "Ami ID is $AMI_ID "
 
 echo -n "LAUNCHING THE INSTANCE WITH $AMI_ID AS AMI :"
-aws ec2 run-instances --image-id $AMI_ID --instance-type t2.micro --tag-specifications "ResourceType=instance,Tags=[{Key=webserver,Value=$COMPONENT}]"
+aws ec2 run-instances --image-id $AMI_ID --instance-type t2.micro --tag-specifications "ResourceType=instance,Tags=[{Key=webserver,Value=$COMPONENT}]" | jq
