cfaa1260dc59a85a1ab121208f7d45ab5a942c79
unknown <anilkumarravuri510@gmail.com>
2023-03-22 12:35:57 +0530

    auto push

--- robot/components/mongodb.sh
+++ robot/components/mongodb.sh
@@ -3,24 +3,27 @@
 echo "i am mongodb"
 
 COMPONENT=mongodb
-LOGFILE="/tmp/$COMPONENT.log"
 
-#set -e
-#validating wether the executed user is root user or not
-ID=$(id -u)
-if [ "$ID" -ne 0 ] ; then 
-   echo -e "\e[31m you should excute this script as a root user or with sudo as a prefix \e[0m"  
-   exit 1
-fi
+source components/common.sh 
 
-stat() {
-if [ $1 -eq 0 ] ; then
-   echo -e "\e[32m success \e[0m"
-else
-   echo -e "\e[31m failure \e[0m"
-   exit 2
-fi
-}
+# LOGFILE="/tmp/$COMPONENT.log"
+
+# #set -e
+# #validating wether the executed user is root user or not
+# ID=$(id -u)
+# if [ "$ID" -ne 0 ] ; then 
+#    echo -e "\e[31m you should excute this script as a root user or with sudo as a prefix \e[0m"  
+#    exit 1
+# fi
+
+# stat() {
+# if [ $1 -eq 0 ] ; then
+#    echo -e "\e[32m success \e[0m"
+# else
+#    echo -e "\e[31m failure \e[0m"
+#    exit 2
+# fi
+# }
 
 echo -n "CONGIGURING $COMPONENT REPO :"
 curl -s -o /etc/yum.repos.d/$COMPONENT.repo https://raw.githubusercontent.com/stans-robot-project/$COMPONENT/main/mongo.repo 
