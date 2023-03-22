3f957eb731be24a6497bdffc6489e5e2bfc4ff73
unknown <anilkumarravuri510@gmail.com>
2023-03-22 23:57:18 +0530

    auto push

--- robot/create-ec2.sh
+++ robot/create-ec2.sh
@@ -4,3 +4,6 @@
 
 AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=DevOps-LabImage-CentOS7" | jq '.Images[].ImageId' |sed -e 's/"//g')
 echo "Ami ID is $AMI_ID "
+
+echo -n "LAUNCHING THE INSTANCE WITH $AMI_ID AS AMI :"
+aws ec2 run-instances --image-id $AMI_ID --instance-type t2.micro
