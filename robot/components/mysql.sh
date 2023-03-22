782f793f7aa0c71941c3fbad181e273fe7821fd8
unknown <anilkumarravuri510@gmail.com>
2023-03-22 10:49:24 +0530

    auto push

--- robot/components/mysql.sh
+++ robot/components/mysql.sh
@@ -1,3 +1,21 @@
 #!/bin/bash
 
 echo "i am mysql"
+
+COMPONENT=mysql
+source components/common.sh
+
+
+echo -n "CONFIGURING $COMPONENT REPO :"
+curl -s -L -o /etc/yum.repos.d/mysql.repo https://raw.githubusercontent.com/stans-robot-project/mysql/main/mysql.repo  &>> $LOGFILE
+stat $?
+
+echo -n "INSTALLING $COMPONENT :"
+yum install mysql-community-server -y  &>> $LOGFILE
+stat $?
+
+echo -n "ENABLING $COMPONENT :"
+systemctl enable mysqld  &>> $LOGFILE
+systemctl start mysqld    &>>  $LOGFILE
+stat $?
+
