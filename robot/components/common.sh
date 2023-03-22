d1e6fd9545540709a445070de94bacfcef108d9e
unknown <anilkumarravuri510@gmail.com>
2023-03-22 14:59:43 +0530

    auto push

--- robot/components/common.sh
+++ robot/components/common.sh
@@ -96,7 +96,7 @@ PYTHON() {
     GROUPID=$(id -g roboshop)
     echo -n "UPDATING $COMPONENT.ini FILE :"
     #sed -i -e "/^uid/ c uid=${USERID}" -e "/^gid/ c gid=${GROUPID}"  /home/$APPUSER/$COMPONENT/$COMPONENT.ini
-    sed -i -e "/^uid/ c uid=${USERID}" -e "/^gid/ c gid=${GROUPID}" /home/$APPUSER/$COMPONENT/$COMPONENT.ini  &>> $LOGFILE
+    sed -i -e "/^uid/ c uid=${USERID}" -e "/^gid/ c gid=${GROUPID}" /home/$APPUSER/$COMPONENT/$COMPONENT.ini  
 
     # CALLING CONFIG_SVC FUNCTION
     CONFIG_SVC
