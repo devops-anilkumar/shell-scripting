225964dfc64e5182152f5adfbd854543b140c7be
unknown <anilkumarravuri510@gmail.com>
2023-03-22 12:37:05 +0530

    auto push

--- robot/components/redis.sh
+++ robot/components/redis.sh
@@ -3,24 +3,26 @@
 echo "i am redis"
 
 COMPONENT=redis
-LOGFILE="/tmp/$COMPONENT.log"
 
-set -e
-#validating wether the executed user is root user or not
-ID=$(id -u)
-if [ "$ID" -ne 0 ] ; then 
-   echo -e "\e[31m you should excute this script as a root user or with sudo as a prefix \e[0m"  
-   exit 1
-fi
+source components/common.sh
 
-stat() {
-if [ $1 -eq 0 ] ; then
-   echo -e "\e[32m success \e[0m"
-else
-   echo -e "\e[31m failure \e[0m"
-   exit 2
-fi
-}
+#LOGFILE="/tmp/$COMPONENT.log"
+# set -e
+# #validating wether the executed user is root user or not
+# ID=$(id -u)
+# if [ "$ID" -ne 0 ] ; then 
+#    echo -e "\e[31m you should excute this script as a root user or with sudo as a prefix \e[0m"  
+#    exit 1
+# fi
+
+# stat() {
+# if [ $1 -eq 0 ] ; then
+#    echo -e "\e[32m success \e[0m"
+# else
+#    echo -e "\e[31m failure \e[0m"
+#    exit 2
+# fi
+# }
 
 echo -n "CONFIGURING $COMPONENT REPO :"
 curl -L https://raw.githubusercontent.com/stans-robot-project/redis/main/redis.repo -o /etc/yum.repos.d/redis.repo   &>>  $LOGFILE
