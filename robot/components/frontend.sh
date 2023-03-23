da8fe2402f9faa75e4fe87dd5d48e9abed24c900
unknown <anilkumarravuri510@gmail.com>
2023-03-24 01:46:56 +0530

    auto push

--- robot/components/frontend.sh
+++ robot/components/frontend.sh
@@ -44,10 +44,10 @@ rm -rf frontend-main README.md
 mv localhost.conf /etc/nginx/default.d/roboshop.conf
 stat $?
 
-for component in catalogue cart user shipping payment; do
-     echo -n "updating the proxy details in the reverse proxy file :"
-     sed -i "/$component/s/localhost/$component.roboshop.internal/" /etc/nginx/default.d/roboshop.conf
-done
+# for component in catalogue cart user shipping payment; do
+#      echo -n "updating the proxy details in the reverse proxy file :"
+#      sed -i "/$component/s/localhost/$component.roboshop.internal/" /etc/nginx/default.d/roboshop.conf
+# done
 
 echo -n "STARTING THE SERVICE :"
 systemctl enable nginx &>> $LOGFILE
